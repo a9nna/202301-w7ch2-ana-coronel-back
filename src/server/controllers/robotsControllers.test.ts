438274cd55eae8f23c4eef63dc2005339d9f91b7
Marc Girbau <marc.girbau94@gmail.com>
2023-02-18 17:42:49 +0100

    Refactor robotsController test

--- src/server/controllers/robotsControllers.test.ts
+++ src/server/controllers/robotsControllers.test.ts
@@ -1,18 +1,22 @@
 /* eslint-disable @typescript-eslint/consistent-type-assertions */
 import { type Response, type Request, type NextFunction } from "express";
+import { Robot } from "../../database/models/Robot.js";
 import { getRobots } from "./robotsControllers.js";
+
+beforeEach(() => jest.restoreAllMocks());
 
 describe("Given a getRobots controller", () => {
   describe("When it receives a response", () => {
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as Partial<Response>;
-    const req = {} as Request;
-    const next: NextFunction = jest.fn();
-
     test("Then it should call its status method with 200", async () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({}),
+      } as Partial<Response>;
+      const req = {} as Request;
+      const next = jest.fn();
       const expectedStatusCode = 200;
+
+      Robot.find = jest.fn().mockReturnValue({});
 
       await getRobots(req, res as Response, next);
 
@@ -20,9 +24,17 @@ describe("Given a getRobots controller", () => {
     });
 
     test("Then it should call its json method", async () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({}),
+      } as Partial<Response>;
+      const req = {} as Request;
+      const next = jest.fn();
+      Robot.find = jest.fn().mockReturnValue({});
+
       await getRobots(req, res as Response, next);
 
-      expect(res.json).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalledWith({ robots: {} });
     });
   });
 });
