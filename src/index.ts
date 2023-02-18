dbd252cd797daf3c1695a1a7ee6860796014dfde
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-18 13:11:51 +0100

    Set up connection to data base

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import createDebug from "debug";
 import startServer from "./server/startServer.js";
+import connectDataBase from "./database/connectDataBase.js";
 
 const debug = createDebug("robots:root");
 
 const port = process.env.PORT ?? 4000;
+const mongoDdUrl = process.env.MONGODB_CONNECTION_URL;
 
 try {
   await startServer(+port);
   debug(`Server listening on port ${port}`);
+
+  await connectDataBase(mongoDdUrl!);
+  debug("Connected to data base");
 } catch (error) {
   debug(error.message);
 }
