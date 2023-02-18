0fc5c40fbb214c7b812d90c440c9f077f00ffc1e
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-18 23:32:35 +0100

    Add testing for getById controller

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,4 +4,14 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
   resolver: "jest-ts-webcompat-resolver",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "src/**/*.tsx",
+    "!src/index.tsx",
+    "!src/react-app-env.d.ts",
+    "!src/setupTests.ts",
+    "!src/server/index.ts",
+    "!src/server/startServer.ts",
+    "!src/loadEnvironment.ts",
+  ],
 };
--- src/server/controllers/robotsControllers.test.ts
+++ src/server/controllers/robotsControllers.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/consistent-type-assertions */
 import { type Response, type Request, type NextFunction } from "express";
 import { Robot } from "../../database/models/Robot.js";
-import { getRobots } from "./robotsControllers.js";
+import { getRobotById, getRobots } from "./robotsControllers.js";
 
 beforeEach(() => jest.restoreAllMocks());
 
@@ -35,6 +35,37 @@ describe("Given a getRobots controller", () => {
       await getRobots(req, res as Response, next);
 
       expect(res.json).toHaveBeenCalledWith({ robots: {} });
+    });
+
+    test("Then it should call its status method with 200", async () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({}),
+      } as Partial<Response>;
+      const req = {} as Request;
+      const next = jest.fn();
+      const expectedStatusCode = 200;
+
+      Robot.findById = jest.fn().mockReturnValue({});
+
+      await getRobotById(req, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should call its json method", async () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({}),
+      } as Partial<Response>;
+      const req = {} as Request;
+      const next = jest.fn();
+
+      Robot.findById = jest.fn().mockReturnValue({});
+
+      await getRobotById(req, res as Response, next);
+
+      expect(res.json).toHaveBeenCalledWith({ robot: {} });
     });
   });
 });
