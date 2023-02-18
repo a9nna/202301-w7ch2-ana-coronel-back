16f78f19a4a002375fd7f1b2ae05a9b47e0c6598
Marc Girbau <marc.girbau94@gmail.com>
2023-02-18 11:22:26 +0100

    Create startServer function to start the server

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,13 +6,26 @@ module.exports = {
   extends: ["xo", "prettier"],
   overrides: [
     {
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+      },
       extends: ["xo-typescript", "prettier"],
       files: ["*.ts", "*.tsx"],
+    },
+    {
+      files: ["ssrc/**/models/**/*.ts"],
+      rules: { "@typescript-eslint/naming-convention": "off" },
     },
   ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "new-cap": ["error", { capIsNewExceptions: ["Router"] }],
+    "no-implicit-coercion": "off",
+  },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
 import express from "express";
+import createDebug from "debug";
 
-const app = express();
-
-const port = 4000;
-
-app.get("/robots", (req, res) => {
-  res.status(200).json({ pong: true });
-});
-
-app.listen(port);
+const debug = createDebug("robots:root");
