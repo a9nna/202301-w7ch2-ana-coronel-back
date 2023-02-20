7fb6e1c648ada642009db2905053bdb33a83fbed
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-20 19:02:59 +0100

    Create usersControllers login

---
+++ src/server/controllers/usersControllers.ts
@@ -0,0 +1,42 @@
+import "../../loadEnvironment.js";
+import { type NextFunction, type Request, type Response } from "express";
+import { CustomError } from "../../CustomError/CustomError.js";
+import User from "../../database/models/User.js";
+import { type UserCredentials } from "../../types";
+import jwt from "jsonwebtoken";
+
+const loginUser = async (
+  req: Request<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    UserCredentials
+  >,
+  res: Response,
+  next: NextFunction
+) => {
+  const { userName, password } = req.body;
+
+  const user = await User.findOne({ userName, password });
+
+  if (!user) {
+    const customError = new CustomError(
+      "Wrong credentials",
+      401,
+      "Wrong credentials"
+    );
+
+    next(customError);
+
+    return;
+  }
+
+  const jwtPayload = {
+    sub: user?._id,
+  };
+
+  const token = jwt.sign(jwtPayload, process.env.JWT_SECRET!);
+
+  res.status(200).json({ token });
+};
+
+export default loginUser;
